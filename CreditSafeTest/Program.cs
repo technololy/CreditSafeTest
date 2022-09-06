ce8bde7d49684d6f4cdfbc0d902dce1776ee21b5
ololadeoyebanji <loladeking@hotmail.com>
2022-09-06 18:00:55 +0100

    first commit

---
+++ CreditSafeTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreditSafeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "HeLovesCheeseBiscuits";
+            var response = CreateSpaceInWords(inputString);
+            Console.WriteLine(response);
+
+        }
+
+        private static string CreateSpaceInWords(string input)
+        {
+            string totalWord = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                var character = input[i]; 
+                if (char.IsUpper(character))
+                {
+                    totalWord += " ";
+                    totalWord += character.ToString();
+                }
+                else
+                {
+                    totalWord += character.ToString();
+                }
+            }
+            return totalWord;
+        }
+    }
+}
+
